e61bfeb4dbacb78c10ff14c9a5104883790cac14
AHoobler <ashley.hoobler@gmail.com>
2018-02-09 11:30:13 -0500

    added volunteer class

--- VPShelter/Volunteer.cs
+++ VPShelter/Volunteer.cs
@@ -8,6 +8,35 @@ namespace VPShelter
 {
     public class Volunteer : Employee
     {
+        private string volHours;
 
+        // additional property
+        public string VolHours
+        {
+            get { return this.volHours; }
+            set { this.volHours = value; }
+        }
+
+
+        //Constructor
+        public Volunteer()
+        {
+            //default
+        }
+
+        public void FeedPet()
+        {
+            Console.WriteLine("You have fed the pet.");
+        }
+
+        public void WaterPet()
+        {
+            Console.WriteLine("You gav the pet water.");
+        }
+
+        public override void ClockIn()
+        {
+            Console.WriteLine("You have clocked in as a volunteer.");
+        }
     }
 }
