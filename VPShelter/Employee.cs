a6ff8c72821d15fa3dfda8a3e61f333ad6fa246d
AHoobler <ashley.hoobler@gmail.com>
2018-02-09 11:04:11 -0500

    added employee class

---
+++ VPShelter/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPShelter
+{
+    abstract class Employee
+    {
+        //field
+        private int employeeID;
+
+
+        // one property
+        public int EmployeeID
+        { 
+        get { return employeeID; }
+        }
+
+
+        // no constructor in abstract class
+
+        // two abstract methods
+       public virtual void FeedPet()
+        {
+            Console.WriteLine("You fed the pet");
+        }
+
+        public virtual void WaterPet()
+        {
+            Console.WriteLine("You gave the pet water");
+        }
+    }
+}
