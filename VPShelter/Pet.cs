460d0d1ae2fb766241785d2681eff3d90ff6e879
AHoobler <ashley.hoobler@gmail.com>
2018-02-09 10:52:14 -0500

    added pet class

---
+++ VPShelter/Pet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPShelter
+{
+    public class Pet : VirtualPet
+    {
+        private string pType;
+        private string pDiet;
+
+        public string PDiet //UPPERCASE
+        {
+            get { return this.pDiet; }
+            set { this.pDiet = value; }
+        }
+
+        public Pet()
+        {
+            // default
+        }
+
+        public Pet(string pType, string pDiet)
+        {
+            this.pDiet = pDiet;
+            this.pType = pType;
+        }
+    }
+}
