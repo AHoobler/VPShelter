46cfb28a32ad74a7112b47a3d84bb2d46416eab6
AHoobler <ashley.hoobler@gmail.com>
2018-02-10 15:07:01 -0500

    fixed the adoption method

--- VPShelter/Manager.cs
+++ VPShelter/Manager.cs
@@ -29,15 +29,33 @@ namespace VPShelter
         {
             Console.WriteLine();
             Console.WriteLine("Which pet would you like to find a home for?");
-            Console.WriteLine("Sirius \n Crookshanks \n Trevor \n Hedwig");
+            Console.WriteLine("\n Sirius \n Crookshanks \n Trevor \n Hedwig");
+            Console.WriteLine();
             petAdopt = Console.ReadLine().ToLower();
 
-            if (petAdopt == "sirius") ;
+            switch (petAdopt)
             {
+                case "sirius":            
+                 Console.WriteLine("You found Sirius a home!");
+                    Console.WriteLine();
+                    return;
 
+                case "crookshanks":
+                   Console.WriteLine("You found Crookshanks a home!");
+                    Console.WriteLine();
+                    return;
+
+                case "trevor":
+                    Console.WriteLine("You found Trevor a home!");
+                    Console.WriteLine();
+                    return;
+
+                case "Hedwig":
+                    Console.WriteLine("You found a home for Hedwig");
+                    Console.WriteLine();
+                    return;
             }
-
-
+            
         }
 
         public override void ClockIn()
